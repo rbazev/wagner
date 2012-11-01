d748f8d02f3781be92758bf8eb66adb16251190e
awhitlock <amandabwhitlock@gmail.com>
2012-11-01 18:01:30 -0300

    Stabilizing selection fitness calc method added

--- genotype.py
+++ genotype.py
@@ -292,7 +292,10 @@ class Genotype(object):
             newmatrix[x] = row #add row x of the parent chosen to the offspring's genotype
         offspring = Genotype(newmatrix)
         return offspring
-
+    
+    def calc_stab_sel_fitness(self, stabilizing_selection_strength):
+        D = calculate_equilibrium_steady_state(average_expression_pattern, gene_expression_pattern, n_genes) #each of these is precalc in development object
+        self.stabilized_fitness = math.log(D/stabilizing_selection_strength)
 
     def generate_random_initial_expression_state(self):
         '''
