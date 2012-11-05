9db3e29a56b75348ae046239dc10f81b49cc65b6
awhitlock <amandabwhitlock@gmail.com>
2012-11-05 00:09:31 -0500

    reproduce population methods

--- population.py
+++ population.py
@@ -1,5 +1,5 @@
 """
-genotype.py: implementation of the gene network model described in Siegal & Bergman (2002) Waddington's canalization
+population.py: implementation of the gene network model described in Siegal & Bergman (2002) Waddington's canalization
     revisited: developmental stability and evolution. PNAS 99: 10528-32.
 
 Contains the Population class.
@@ -24,43 +24,36 @@ class Population(object):
   Attributes: population_size
   """
   @staticmethod
-  def found_population(population_size):
+  def found_population(population_size, n_genes, connectivity):
     founding_pop = Population()
     founding_pop.population_size = population_size
+    Genotype.n_genes = n_genes
+    Genotype.connectivity = connectivity
     founder = Genotype.generate_random(Genotype.n_genes, Genotype.connectivity)
     founding_pop.organisms = []
     popsize = 0
+    organism_num = 0
     while organism_num < population_size:
       founding_pop.organisms.append(founder)
       organism_num = organism_num + 1
-    return founding.pop 
+    return founding_pop 
   
   def set_stabilizing_selection(self, stabilizing_selection_strength):
     self.stabilizing_selection_strength = stabilizing_selection_strength
         
-    
-  def sexually_reproduce_pop (self, N):
-    new_pop = []
-    organism = 0
-    while organism < N:
-      genotype1 = choose_genotype #choose_genotype is the random-weighted selection method
-      genotype2 = choose_genotype
-      rec_offspring = recombine(genotype1, genotype2)
-      mut_offspring = rec_offspring.mutate_random(rnd.poisson(rec_offspring.mutation_rate))
-      new_pop.append(mut_offspring)
-      organism = organism + 1
-    return new_pop
-        
-  def choose_offspring (self):   
+  def choose_genotype(self):   
     sumfitness = 0      #sum of the fitnesses of every genotype in pop, to be used as top range for generating rand number
     for genotype in self.organisms:
       sumfitness += genotype.fitness     
     rand = random.randrange(0, sumfitness)
     total = 0       #will iterate over genotypes, summing fitnesses until reaching random number
-    for genotype in self.organisms:
+    gen = 0
+    while gen < len(self.organisms):
       total += Genotype.fitness
-      if total > rand
-        return genotype
+      gen = gen + 1
+      if total > rand:
+        return self.organisms[genotype]
+
     
   def get_next_generation_asexual(self):
     next_generation = Population() 
@@ -69,7 +62,7 @@ class Population(object):
     next_generation.organisms = []
     organism_num = 0
     while organism_num < next_generation.population_size:
-      randchoice = self.choose_organism
+      randchoice = self.choose_genotype
       mut_offspring = Genotype.randchoice.mutate_random(rnd.poisson(Genotype.randchoice.mutation_rate))
       next_generation.organisms.append(mut_offspring)
       organism_num = organism_num + 1
@@ -87,3 +80,5 @@ class Population(object):
       mut_offspring = Genotype.rec_offspring.mutate_random(rnd.poisson(Genotype.rec_offspring.mutation_rate))
       next_generation.organisms.append(mut_offspring)
       organism_num = organism_num + 1
+        
+
